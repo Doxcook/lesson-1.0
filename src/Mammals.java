27373e1014863ea5625c829c04579d3ac46c8b7d
egorp <egorpanov4002rus@gmail.com>
2024-09-05 17:44:09 +0300

    1

---
+++ src/Mammals.java
@@ -0,0 +1,29 @@
+public class Mammals {
+    int age;
+    String name;
+
+    public Mammals(){
+        age = 0;
+        name = "none";
+    }
+    public Mammals(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
