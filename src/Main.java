e4c2f814ecee10c7301922053ca418e3f9b131c0
User <apollinariya.klyomina.05@mail.ru>
2024-09-11 17:37:31 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import animal.Mammals;
 import animal.Dog;
 import animal.Cat;
-//1234567890
+//123456
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat(1, "Musa");
